e355a922764fe431172350da233ef78c732e28b7
katlynj15 <bookkateyj@gmail.com>
2023-01-06 23:44:36 +0000

    js group project

--- src/app.js
+++ src/app.js
@@ -7,5 +7,16 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
   console.log("Hello Rigo from the console!");
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let x = 0; x < adj.length; x++) {
+      for (let z = 0; z < noun.length; z++) {
+        console.log(pronoun[i] + adj[x] + noun[z] + ".com");
+      }
+    }
+  }
 };
